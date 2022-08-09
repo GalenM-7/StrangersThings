2203e0a403dcfc74fa9c85172c54c97ab0810a03
Galen Montrone <gjmontrone@gmail.com>
2022-08-08 20:22:40 -0500

    messages showing up

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 
-const Profile = ({ loggedIn, getLoggedIn, currentUser, setCurrentUser }) => {
+const Profile = ({ loggedIn, setLoggedIn, posts, setPosts, filteredPosts, setFilteredPosts, filter, setFilter, currentUser, setCurrentUser }) => {
 
     let url = "https://strangers-things.herokuapp.com/api/2206-ftb-et-web-ft-b";
+
+     const [ receivedMessagesToggle, setReceivedMessagesToggle] = useState(false);
+    const [ sentMessagesToggle, setSentMessagesToggle] = useState(false);
+    const [ receivedMessages, setReceivedMessages] = useState([]);
+    const [ sentMessages, setSentMessages] = useState([]);
+
 
     const checkUser = localStorage.getItem("token");
     console.log(checkUser)
@@ -32,6 +38,20 @@ const Profile = ({ loggedIn, getLoggedIn, currentUser, setCurrentUser }) => {
       getCurrentUser();
     }
 
+    async function getSentMessages() {
+      try {
+        const result = await fetch(`${url}/posts`);
+        console.log(result)
+        const resultParsed = await result.json();
+        console.log(resultParsed.data.posts);
+        setPosts(resultParsed.data.posts);
+        console.log("result.data.posts.id")
+        console.log(resultParsed.data.posts[0]._id);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
     function getProfile(){
         if ( !checkUser) {
             return (     
@@ -55,14 +75,59 @@ const Profile = ({ loggedIn, getLoggedIn, currentUser, setCurrentUser }) => {
                 <div className="sectionHeader">
                     <h1 className="postsTitle">Profile</h1>
                     <div>
-                        <button value={"sent"} className="togglePostButton">See Sent Messages</button>
-                        <button value={"recieved"} className="togglePostButton">See Received Messages</button>
+                        <button value={"sent"} className="togglePostButton" onClick={async ()=>{
+                          setSentMessagesToggle(true);
+                          setReceivedMessagesToggle(false);
+                             try {
+                                const result = await fetch(`${url}/users/me`,{
+                                  headers: {
+                                    "Content-Type": "application/json",
+                                    "Authorization": `Bearer ${checkUser}`
+                                  }
+                                });
+                                console.log(result)
+                                const resultParsed = await result.json();
+                                const sentMessagesFiltered = [];
+                                const receivedMessagesFiltered = [];
+
+                                for ( let i = 0; i < resultParsed.data.messages.length; i++) {
+                                  if (resultParsed.data.messages[i].fromUser.username === currentUser){
+                                    sentMessagesFiltered.push(resultParsed.data.messages[i])
+                                    console.log(resultParsed.data.messages[i])
+                                  } else {
+                                    receivedMessagesFiltered.push(resultParsed.data.messages[i])
+                                  }
+                                };
+
+                                setSentMessages(sentMessagesFiltered);
+                                setReceivedMessages(receivedMessagesFiltered);
+                                console.log("sentMessages");
+                                console.log(sentMessagesFiltered);
+                                console.log("resultParsed.dataworking");
+                                console.log(resultParsed.data.messages);
+                                    console.log("currentUsername");
+                                console.log(currentUser);
+                                    console.log("resultParsed.datato check");
+                                console.log(resultParsed.data.messages.fromUser);
+                                
+                              } catch (error) {
+                                console.log(error);
+                              }
+                        }}>See Sent Messages</button>
+                        <button value={"recieved"} className="togglePostButton" onClick={()=>{
+                          setReceivedMessagesToggle(true);
+                          setSentMessagesToggle(false);
+                        }}>See Received Messages</button>
                     </div>
                 </div>
-                    <h1>Received Messages</h1>
-
-
-                    <h1>Sent Messages</h1>
+                {
+                  sentMessagesToggle ? sentMessages.map((message, idx) => { 
+                    return <div className="postItem" key = {idx}><h1>{message.content}</h1><h2>Post Title: {message.post.title}</h2></div>
+                  }) : null
+                }
+                {
+                  receivedMessagesToggle ? <div></div> : null
+                }
               </div>
             </div>
           )
